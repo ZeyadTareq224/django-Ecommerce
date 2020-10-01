4e5578c7b387f30744e94cfb59f0e325e7f3a7e6
Ziyad Tareq <60711983+ZeyadTareq224@users.noreply.github.com>
2020-10-01 16:42:41 +0200

    added remove from cart feature

--- Ecom/urls.py
+++ Ecom/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path('product-detail/<int:id>', views.product_details, name='products_details'),
 	path('checkout/', views.checkout, name='checkout'),
 	path('add_to_cart/<int:id>', views.add_to_cart, name='add_to_cart'),
+	path('remove_from_cart/<int:id>', views.remove_from_cart, name='remove_from_cart'),
 ]
--- Ecom/views.py
+++ Ecom/views.py
@@ -3,23 +3,47 @@ from .models import Item, Order, OrderItem
 from django.contrib import messages
 from django.utils import timezone
 
-def products_list(request):
 
+def get_total_quantity_in_cart(request):
+	current_cart = Order.objects.get(user=request.user)
+	items = current_cart.items.all()
+
+	total_quantity = 0
+	for item in items:
+		total_quantity += item.quantity
+
+	return total_quantity
+	
+
+def products_list(request):
+	
+	
+	total_quantity = get_total_quantity_in_cart(request)
+	
 	context = {
-	'items': Item.objects.all()
+	'total_quantity': total_quantity,
+	'items': Item.objects.all(),
 	}
 	return render(request, 'Ecom/home-page.html', context)
 
 def	product_details(request, id):
+	#nav cart items
+	total_quantity = get_total_quantity_in_cart(request)
+	#end nav cart items
+
 	item = get_object_or_404(Item, id=id)
 	context = {
-	'item': item
+	'item': item,
+	'total_quantity': total_quantity
 	}
 	return render(request, 'Ecom/product-page.html', context)
 
 def checkout(request):
+	#nav cart items
+	total_quantity = get_total_quantity_in_cart(request)
+	#end nav cart items
 
-	context = {}
+	context = {'total_quantity': total_quantity}
 	return render(request, 'Ecom/checkout-page.html', context)
 
 
@@ -38,15 +62,41 @@ def add_to_cart(request, id):
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "This item quantity was updated.")
-            return redirect("products_list")
+            return redirect("products_details", id=id)
         else:
             order.items.add(order_item)
             messages.info(request, "This item was added to your cart.")
-            return redirect("products_list")
+            return redirect("products_details", id=id)
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
             user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "This item was added to your cart.")
-        return redirect("products_list")
+        return redirect("products_details", id=id)
+
+def remove_from_cart(request, id):
+    item = get_object_or_404(Item, id=id)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__id=item.id).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            order_item.delete()
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("products_details", id=id)
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("products_details", id=id)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("products_details", id=id)
